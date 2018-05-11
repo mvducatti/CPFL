a824a9563e5487a5bc1e2d6bf93864b6308d4422
Marcos Ducatti <mvducatti@gmail.com>
2018-05-10 22:00:59 -0300

    Cadastro de usuarios funcionando

--- app/src/main/java/com/example/marcos/cpfl/CadastroUsuario.java
+++ app/src/main/java/com/example/marcos/cpfl/CadastroUsuario.java
@@ -1,13 +1,112 @@
 package com.example.marcos.cpfl;
 
+import android.annotation.SuppressLint;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.example.marcos.cpfl.Connection.Constants;
+import com.example.marcos.cpfl.Connection.RequestHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CadastroUsuario extends AppCompatActivity {
+
+    private EditText editTextCPF, editTextNome, editTextSenha, editTextRua, editTextNumero, editTextBairro, editTextCidade, editTextEstado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        editTextCPF = findViewById(R.id.txtCPF);
+        editTextNome = findViewById(R.id.txtNome);
+        editTextSenha = findViewById(R.id.txtSenha);
+        editTextRua = findViewById(R.id.txtRua);
+        editTextNumero = findViewById(R.id.txtNumero);
+        editTextBairro = findViewById(R.id.txtBairro);
+        editTextCidade = findViewById(R.id.txtCidade);
+        editTextEstado = findViewById(R.id.txtEstado);
+
     }
+
+    public void registerUser(View view) {
+
+
+        final String cpf = editTextCPF.getText().toString().trim();
+        final String nome = editTextNome.getText().toString().trim();
+        final String senha = editTextSenha.getText().toString().trim();
+        final String rua = editTextRua.getText().toString().trim();
+        final String numero = editTextNumero.getText().toString().trim();
+        final String bairro = editTextBairro.getText().toString().trim();
+        final String cidade = editTextCidade.getText().toString().trim();
+        final String estado = editTextEstado.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_REGISTER,
+                new com.android.volley.Response.Listener<String>(){
+                    @SuppressLint("ResourceType")
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            if (!obj.getBoolean("error")){
+
+                                Snackbar.make(findViewById(android.R.id.content),obj.getString("message"),Snackbar.LENGTH_LONG).show();
+
+                                editTextCPF.setText("");
+                                editTextNome.setText("");
+                                editTextRua.setText("");
+                                editTextSenha.setText("");
+                                editTextNumero.setText("");
+                                editTextBairro.setText("");
+                                editTextCidade.setText("");
+                                editTextEstado.setText("");
+
+                            }else{
+                                Snackbar.make(findViewById(android.R.id.content),obj.getString("message"),Snackbar.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            Toast.makeText(getApplicationContext(), "Erro: " + e,
+                                    Toast.LENGTH_LONG).show();
+
+                        }
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Erro: " + error,
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("cpf", cpf);
+                params.put("username", nome);
+                params.put("password", senha);
+                params.put("street", rua);
+                params.put("house_number", numero);
+                params.put("neighborhood", bairro);
+                params.put("city", cidade);
+                params.put("state", estado);
+                params.put("user_type", String.valueOf(1));
+                return params;
+            }
+        };
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+    }
+
 }
---
+++ app/src/main/java/com/example/marcos/cpfl/Connection/Constants.java
@@ -0,0 +1,21 @@
+package com.example.marcos.cpfl.Connection;
+
+public class Constants {
+
+    //    conexão principal
+    private static final String ROOT_URL = "http://10.133.64.122:8080/cpfl/v1/";
+
+    //    conexão secundária acessando os folders users e news
+    private static final String ROOT_USERS= "users/";
+//    private static final String ROOT_NEWS = "news/";
+
+//    acessando os métodos de POST e GET utilizando ROOT_URL + ROOT_USERS ou ROOT_NEWS e o restante to path em ""
+
+    //    CALLS DE USER
+    public static final String URL_REGISTER = ROOT_URL+ROOT_USERS+"createUser.php";
+//    public static final String URL_LOGIN = ROOT_URL+ROOT_USERS+"userLogin.php";
+
+    //    CALLS DE NOTICIAS
+//    public static final String URL_REGISTER_NEWS = ROOT_URL+ROOT_NEWS+"registerNews.php";
+//    public static final String URL_SHOW_NEWS = ROOT_URL+ROOT_NEWS+"getAllNews.php";
+}
